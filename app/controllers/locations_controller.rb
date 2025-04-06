f3e10280b578e08df9515c50d8d892e969b5df55
HilaryOkello <hilaryokello091@gmail.com>
2025-04-06 20:55:41 +0300

    feat: enhance locations controller to render locations in index and show actions

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,14 @@
 class LocationsController < ApplicationController
     def index
-        render inertia: 'LocationList'
+        render inertia: 'LocationList', props: {
+            locations: current_user.locations
+        }
     end
+
+    def show
+        render inertia: 'Location', props: {
+            location: current_user.locations.find(params[:id]),
+        }
+    end
+
 end
