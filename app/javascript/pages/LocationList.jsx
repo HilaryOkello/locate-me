1f8b4d7b0b5ccf39830efb32330ef6901a6b7942
HilaryOkello <hilaryokello091@gmail.com>
2025-04-06 20:56:18 +0300

    feat: update LocationList component to display a list of locations

--- app/javascript/pages/LocationList.jsx
+++ app/javascript/pages/LocationList.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from '@inertiajs/react'
 
-const LocationList = ({user}) => {
-   return <h1>{user.email}'s Locations</h1>
+const LocationList = ({user, locations}) => {
+   return (
+      <>
+         <h1>{user.email}'s Locations</h1>
+        {locations.map(location => (
+            <div key={location.id}>
+               <Link href={`/locations/${location.id}`}>{location.name}</Link>
+            </div>   
+         ))}
+      </>
+   ) 
 };
 
 export default LocationList;
