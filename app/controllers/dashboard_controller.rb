1719f1e8d8a0710e911dd4da38bc61854e9149fe
HilaryOkello <hilaryokello091@gmail.com>
2025-04-07 23:14:46 +0300

    refactor DashboardController to include user data in index and enhance user deletion logic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,19 +3,22 @@ class DashboardController < ApplicationController
 
   def index
     @locations = Location.includes(:user).all
+    @users = User.all
     @total_locations = Location.count
     @total_users = User.count
 
     render inertia: "Dashboard", props: {
       locations: @locations.as_json(include: { user: { only: [ :id, :name, :email ] } }),
+      users: @users.as_json(only: [ :id, :name, :email, :created_at ]),
       totalLocations: @total_locations,
-      totalUsers: @total_users
+      totalUsers: @total_users,
+      currentUserId: current_user.id # Pass current user ID to the frontend
     }
   end
 
   def destroy_location
     location = Location.find(params[:id])
-    location_name = location.name # Store the name before deleting
+    location_name = location.name
     location.destroy
 
     redirect_to dashboard_path, notice: "Location '#{location_name}' successfully deleted"
@@ -25,5 +28,27 @@ class DashboardController < ApplicationController
     Location.destroy_all
 
     redirect_to dashboard_path, notice: "All locations successfully deleted"
+  end
+
+  def destroy_user
+    user = User.find(params[:id])
+
+    # Prevent admin from deleting their own account
+    if user.id == current_user.id
+      return redirect_to dashboard_path, alert: "You cannot delete your own account"
+    end
+
+    user_name = user.name
+    user.destroy
+
+    redirect_to dashboard_path, notice: "User '#{user_name}' successfully deleted"
+  end
+
+  def destroy_all_users
+    # Exclude the current admin from the deletion
+    User.where.not(id: current_user.id).destroy_all
+
+    redirect_to dashboard_path, notice: "All users except your account successfully deleted"
+  endirect_to dashboard_path, notice: "All users successfully deleted"
   end
 end
