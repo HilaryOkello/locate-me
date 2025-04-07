021808bd10367ff6612392a6f860c9af8e324862
HilaryOkello <hilaryokello091@gmail.com>
2025-04-07 23:15:15 +0300

    refactor HomeController#index to include user names in location data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
+    locations = Location.all.includes(:user).map do |location|
+      location.as_json.merge(user_name: location.user.name)
+    end
+
     render inertia: "LocationList", props: {
-      locations: Location.all
+      locations: locations
     }
   end
 end
