ed2c1f80f27caaa363b5fe59f3d40722ecdfdbe6
HilaryOkello <hilaryokello091@gmail.com>
2025-04-07 15:41:11 +0300

    feat: implement create action for locations with success and error handling

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,4 +11,23 @@ class LocationsController < ApplicationController
         }
     end
 
+    def create
+        @location = current_user.locations.new(location_params)
+    
+        if @location.save
+          render inertia: 'LocationList', props: {
+            success: 'Location saved successfully!',
+            locations: current_user.locations.all
+          }, url: '/' # Try explicitly setting the URL
+        else
+          render inertia: 'LocationList', props: { errors: @location.errors.full_messages }, status: :unprocessable_entity, url: '/' # And here
+        end
+      end
+
+    private
+
+    def location_params
+        params.require(:location).permit(:name, :latitude, :longitude)
+    end
+
 end
