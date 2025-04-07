934bfaf694eab5ca8461e5006cc4275f1f44630e
HilaryOkello <hilaryokello091@gmail.com>
2025-04-07 17:03:15 +0300

    refactor: remove unused index and show actions from LocationsController

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,26 +1,13 @@
 class LocationsController < ApplicationController
-    def index
-        render inertia: 'LocationList', props: {
-            locations: current_user.locations
-        }
-    end
-
-    def show
-        render inertia: 'Location', props: {
-            location: current_user.locations.find(params[:id]),
-        }
-    end
-
     def create
         @location = current_user.locations.new(location_params)
-    
         if @location.save
           render inertia: 'LocationList', props: {
             success: 'Location saved successfully!',
             locations: current_user.locations.all
-          }, url: '/' # Try explicitly setting the URL
+          }, url: '/' 
         else
-          render inertia: 'LocationList', props: { errors: @location.errors.full_messages }, status: :unprocessable_entity, url: '/' # And here
+          render inertia: 'LocationList', props: { errors: @location.errors.full_messages }, status: :unprocessable_entity, url: '/'
         end
       end
 
