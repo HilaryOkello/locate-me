5cf0923c5967721d6a4c018364fd74563266a8dd
HilaryOkello <hilaryokello091@gmail.com>
2025-04-07 10:49:54 +0300

    refactor: reorganize private methods in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :authenticate_user!
   inertia_share user: -> { current_user }
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path # Redirects to the login page after logout
+  end
 end
