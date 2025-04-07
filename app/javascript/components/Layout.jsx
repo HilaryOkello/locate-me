080a2afe6e2f898bf7672c6d2974dfffb9c3bd6f
HilaryOkello <hilaryokello091@gmail.com>
2025-04-07 10:50:54 +0300

    feat: pass user prop to Navbar in Layout component

--- app/javascript/components/Layout.jsx
+++ app/javascript/components/Layout.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import Navbar from './NavBar';
 import Footer from './Footer';
 
-const Layout = ({ children }) => {
+const Layout = ({ user, children }) => {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-      <Navbar />
+      {/* pass the user prop to navbar */}
+      <Navbar user={user} />
       <main className="flex-grow relative overflow-hidden">
         {children}
       </main>
