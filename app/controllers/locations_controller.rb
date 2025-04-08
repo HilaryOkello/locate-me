13116c54563b504ee705910a3b48d7b8ba3e990a
HilaryOkello <hilaryokello091@gmail.com>
2025-04-08 07:02:49 +0300

    fix: LocationsController#create to use redirect instead of render for successful location creation

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,18 +1,16 @@
 class LocationsController < ApplicationController
-    def create
-        @location = current_user.locations.new(location_params)
-        if @location.save
-          render inertia: "LocationList", props: {
-            success: "Location saved successfully!",
-            locations: current_user.locations.all
-          }, url: "/"
-        else
-          render inertia: "LocationList", props: { errors: @location.errors.full_messages }, status: :unprocessable_entity, url: "/"
-        end
-      end
-    private
-
-    def location_params
-        params.require(:location).permit(:name, :latitude, :longitude)
+  def create
+    @location = current_user.locations.new(location_params)
+    if @location.save
+      redirect_to root_path, success: "Location saved successfully!"
+    else
+      render inertia: "LocationList", props: { errors: @location.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :latitude, :longitude)
+  end
 end
